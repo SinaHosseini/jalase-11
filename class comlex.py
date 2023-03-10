dff58caec683f01abf5da1fd8273a6c68e3c6066
seyed sina hosseini vaez <122821690+SinaHosseini@users.noreply.github.com>
2023-03-10 15:26:27 +0330

    Add files via upload

--- class comlex.py
+++ class comlex.py
@@ -3,18 +3,43 @@ class Complex():
         self.real = real
         self.image = image
 
-
     def show(self):
-        print(self.real, "+", "i", self.image)
+        print(self.real, "+", self.image, "i")
 
-    def sum(self):
-        pass
+    def sum(self, num):
+        new_real = self.real + num.real
+        new_image = self.image + num.image
+        result = Complex(new_real, new_image)
 
-    def sub(self):
-        pass
+        return result
 
-    
+    def sub(self, num):
+        new_real = self.real - num.real
+        new_image = self.image - num.image
+        result = Complex(new_real, new_image)
+
+        return result
+
+    def mul(self, num):
+        # (ac – bd) + (ad + bc)i
+        new_real = self.real * num.real - self.image * num.image
+        new_image = self.real * num.image + self.image * num.real
+        result = Complex(new_real, new_image)
+
+        return result
 
 
-a =Complex(5, 8)
+a = Complex(5, 8)
 a.show()
+
+b = Complex(3, 6)
+b.show()
+
+sum = a.sum(b)
+sum.show()
+
+sub = a.sub(b)
+sub.show()
+
+mul = a.mul(b)
+mul.show()
